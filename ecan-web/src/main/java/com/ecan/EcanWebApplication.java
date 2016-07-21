117f6c3ad3b71d47cc0d92acff03ce22c24c7c93
yiziaoxue <775777006@qq.com>
2016-07-21 11:30:57 +0800

    端口号修改提交

--- ecan-web/src/main/java/com/ecan/EcanWebApplication.java
+++ ecan-web/src/main/java/com/ecan/EcanWebApplication.java
@@ -3,16 +3,24 @@ package com.ecan;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
-public class EcanWebApplication {
+public class EcanWebApplication  extends SpringBootServletInitializer implements EmbeddedServletContainerCustomizer{
 
 	private static Logger logger = Logger.getLogger(EcanWebApplication.class);
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EcanWebApplication.class, args);
 		logger.info("==========服务启动，开始记录==========");
+	}
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		container.setPort(8181);  
 	}
 }
